7edbc138b0d3f123acd6a820bd70a038f3b56cd0
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-07-21 08:55:29 +0200

    Initial commit

---
+++ lib/LandingImagePage.dart
@@ -0,0 +1,111 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class LandingImagePage extends StatefulWidget {
+  @override
+  _LandingImagePageState createState() => _LandingImagePageState();
+}
+
+class _LandingImagePageState extends State<LandingImagePage> {
+  File imageFile;
+  final _picker = ImagePicker();
+
+  Future _openGallery() async {
+    final pickedPath = await _picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      imageFile = File(pickedPath.path);
+    });
+    Navigator.of(context).pop();
+  }
+
+  Future _openCamera() async {
+    final pickedPath = await _picker.getImage(source: ImageSource.camera);
+
+    setState(() {
+      imageFile = File(pickedPath.path);
+    });
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Choose photo from?"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      child: Text(
+                        "Gallery",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
+                        _openGallery();
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: GestureDetector(
+                      child: Text(
+                        "Camera",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
+                        print('hello camera');
+                        _openCamera();
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget presentAccordingly() {
+    if(imageFile == null) {
+      return Text("No Image Selected");
+    }
+    return Image.file(imageFile);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Center(
+              child: Expanded(
+                  child: presentAccordingly(),
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                _showChoiceDialog(context);
+              },
+              child: Text('Select Image'),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _openCamera,
+        tooltip: 'Increment',
+        child: Icon(Icons.camera),
+      ),
+    );
+  }
+}
